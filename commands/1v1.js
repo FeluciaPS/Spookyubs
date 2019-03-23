c076522e3886467f824741d936c052b15f5eafef
TheMezStrikes <TheMezStrikes@gmail.com>
2019-03-24 00:43:51 +0100

    Added 2 1v1 OMs

--- commands/1v1.js
+++ commands/1v1.js
@@ -1,7 +1,18 @@
 
+let canMakeTour = function(room, user) {
+    // I'm gonna use this a lot so why not make a function for it
+    if (room != '1v1') return false;
+    if (!user.can(room, "%")) return false;
+    if (room.tournament) {
+        room.send("A tournament is already going on.");
+        return false;
+    }
+    return true;
+}
+
 module.exports = {
     '1v1': function(room, user, args) {
-        if (!user.can(room, '%')) return;
+        if (!canMakeTour(room, user)) return;
         if (args) {
             if (args[0].startsWith("rr")) {
                 let count = parseInt(args[0].substring(2));
@@ -18,5 +29,22 @@ module.exports = {
             }
         }
         else room.send("/tour create 1v1, elim");
+        if (args[0] === 'o') room.startTour("o"); // Make a tour object manually instead of doing it in parser so the "Official" flag can be passed
     },
+    aaa1v1: 'aaa',
+    aaa: function(room, user, args) {
+        if (!canMakeTour(room, user)) return;
+        let ruleset = "/tour rules Ignore Illegal Abilities, -" + Banlist.aaa['ability-bans'].join(', -') + ", -" + Banlist.aaa['mon-bans'].join(', -'); // Yes I realize this doesn't properly work if there aren't any ability-bans or mon-bans. I'll tackle that if we ever get to that point
+        this['1v1'](room, user, args);
+        room.send(ruleset);
+        room.send("/tour name [Gen 7] AAA 1v1");
+    },
+    ag1v1: 'ag',
+    ag: function(room, user, args) {
+        if (!canMakeTour(room, user)) return;
+        let ruleset = "/tour rules !" + Banlist.ag.join(', !') + ", +" + Banlist['1v1'].join(', +');
+        this['1v1'](room, user, args);
+        room.send(ruleset);
+        room.send("/tour name [Gen 7] AG 1v1");
+    }
 };
